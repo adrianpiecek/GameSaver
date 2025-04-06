157b474a3bda7f2ba13bb9f88577d492020de676
adrianpiecek <adrian.piecek@gmail.com>
2025-04-06 14:02:50 +0200

    new packets and new requirements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module com.gamesaver {
     requires javafx.controls;
     requires javafx.fxml;
+    requires okhttp3;
+    requires java.sql;
+    requires com.fasterxml.jackson.databind;
 
 
     opens com.gamesaver to javafx.fxml;
-    exports com.gamesaver;
+    exports com.gamesaver.app;
+    opens com.gamesaver.app to javafx.fxml;
+    exports com.gamesaver.controller;
+    opens com.gamesaver.controller to javafx.fxml;
 }
